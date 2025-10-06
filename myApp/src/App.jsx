672cf198e4e60a5ac3a67d9f73a7e56d7cfbf74c
Sai Vivek <saivivekbunny69@gmail.com>
2025-10-06 09:37:25 +0530

    react router

--- myApp/src/App.jsx
+++ myApp/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Home from "./components/Home";
+import List from "./components/List";
+import { Link, Route, Routes } from "react-router-dom"
+import Notfound from "./components/Notfound";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>React Router</h1>
+      <Link to="./home">Home</Link><br/>
+      <Link to="./list">List</Link><br/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/home" element={<Home />} />
+        <Route path="/list" element={<List />} />
+        <Route path="*" element={<Notfound/>}/>
+        
+      </Routes>
+      {/* <Home /> */}
+      {/* <List /> */}
+      {/* <Notfound/> */}
+    </div>
+  );
+};
 
-export default App
+export default App;
